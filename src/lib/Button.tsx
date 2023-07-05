ff8b49c35fd9b6c4aa8fe35568fd255caaa5a263
zer0 <osom8979@gmail.com>
2023-07-05 16:55:55 +0900

    Add first spec file

--- src/lib/Button.tsx
+++ src/lib/Button.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import styles from './Button.module.scss';
 
-export class Button extends React.Component {
+export interface ButtonProps {
+  caption?: string;
+}
+
+export class Button extends React.Component<ButtonProps> {
+  static defaultProps = {
+    caption: '',
+  };
+
   render() {
     return (
-      <div className={styles.root}>
-        <span>Button</span>
+      <div className={styles.root} data-testid="root">
+        <span data-testid="caption">{this.props.caption}</span>
       </div>
     );
   }
