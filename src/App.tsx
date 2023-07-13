4647eca60295b77ac1f88aaf48d6444f1418f69e
zer0 <osom8979@gmail.com>
2023-07-13 14:01:32 +0900

    Update osom.run logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import osomRunLogo from './assets/osom.run.svg';
 import styles from './App.module.scss';
 
 function App() {
   return (
     <div className={styles.root}>
       <a href="https://osom.run" target="_blank">
-        <img src={osomRunLogo} alt="osom-ui logo" />
+        <img src="/images/osom.run.svg" alt="osom.run logo" />
       </a>
     </div>
   );
