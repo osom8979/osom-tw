ba8f5916fc40adfd62a690ca8e6744ad4618bc3c
zer0 <osom8979@gmail.com>
2024-03-12 17:20:46 +0900

    Update default config

--- src/tailwind/creator.ts
+++ src/tailwind/creator.ts
@@ -2,54 +2,117 @@ import type {PluginAPI, Config} from 'tailwindcss/types/config';
 
 export const DEFAULT_PREFIX = 'osom-';
 
+export interface OsomTheme {
+  fontSans: string;
+  fontSerif: string;
+  fontMono: string;
+
+  colorBase100: string;
+  colorBase200: string;
+  colorBase300: string;
+  colorBaseContent: string;
+
+  colorPrimary: string;
+  colorPrimaryContent: string;
+  colorSecondary: string;
+  colorSecondaryContent: string;
+  colorInfo: string;
+  colorInfoContent: string;
+  colorSuccess: string;
+  colorSuccessContent: string;
+  colorWarning: string;
+  colorWarningContent: string;
+  colorError: string;
+  colorErrorContent: string;
+
+  sizeCell: string;
+
+  roundedBox: string;
+  roundedBtn: string;
+  roundedBadge: string;
+}
+
 export interface OsomPluginOptions {
+  logs?: boolean;
   prefix?: string;
+
+  light?: Partial<OsomTheme>;
+  dark?: Partial<OsomTheme>;
 }
 
 export function createOsomPlugin(api: PluginAPI, options: OsomPluginOptions): void {
+  console.assert(typeof options !== 'undefined');
+
   const prefix = options.prefix || DEFAULT_PREFIX;
-  const {addComponents, theme} = api;
+  const {addBase, addComponents, theme} = api;
+
+  addBase({
+    ':root': {
+      backgroundColor: theme('colors.base.100'),
+      color: theme('colors.base.content'),
+    },
+  });
+
   addComponents({
     [`.${prefix}btn`]: {
       display: 'flex',
       color: theme('colors.gray.400'),
     },
   });
-  // height: calc(100vh - theme(spacing.12));
 }
 
 export function createOsomConfig(options: OsomPluginOptions): Partial<Config> {
-  const prefix = options.prefix || DEFAULT_PREFIX;
+  console.assert(typeof options !== 'undefined');
   return {
     theme: {
       extend: {
         fontFamily: {
-          sans: ['Noto Sans KR', 'sans-serif', 'system-ui'],
-          serif: ['Hahmlet Variable', 'serif', 'system-ui'],
-          mono: ['Nanum Gothic Coding', 'monospace', 'system-ui'],
+          sans: ['var(--font-sans)', 'Noto Sans KR', 'sans-serif', 'system-ui'],
+          serif: ['var(--font-serif)', 'Hahmlet Variable', 'serif', 'system-ui'],
+          mono: ['var(--font-mono)', 'Nanum Gothic Coding', 'monospace', 'system-ui'],
         },
         colors: {
-          primary: '#96B6C5',
-          secondary: '#ADC4CE',
-          tertiary: '#EEE0C9',
-          quaternary: '#F1F0E8',
-          background: '#F0F0F0',
-          dark: {
-            primary: '#27374D',
-            secondary: '#526D82',
-            tertiary: '#9DB2BF',
-            quaternary: '#DDE6ED',
-            background: '#0f172a',
+          transparent: 'transparent',
+          current: 'currentColor',
+          base: {
+            100: 'rgba(var(--color-base-100), <alpha-value>)',
+            200: 'rgba(var(--color-base-200), <alpha-value>)',
+            300: 'rgba(var(--color-base-300), <alpha-value>)',
+            content: 'rgba(var(--color-base-content), <alpha-value>)',
+          },
+          primary: {
+            DEFAULT: 'rgba(var(--color-primary, <alpha-value>)',
+            content: 'rgba(var(--color-primary-content, <alpha-value>)',
+          },
+          secondary: {
+            DEFAULT: 'rgba(var(--color-secondary, <alpha-value>)',
+            content: 'rgba(var(--color-secondary-content, <alpha-value>)',
+          },
+          info: {
+            DEFAULT: 'rgba(var(--color-info, <alpha-value>)',
+            content: 'rgba(var(--color-info-content, <alpha-value>)',
+          },
+          success: {
+            DEFAULT: 'rgba(var(--color-success, <alpha-value>)',
+            content: 'rgba(var(--color-success-content, <alpha-value>)',
+          },
+          warning: {
+            DEFAULT: 'rgba(var(--color-warning, <alpha-value>)',
+            content: 'rgba(var(--color-warning-content, <alpha-value>)',
+          },
+          error: {
+            DEFAULT: 'rgba(var(--color-error, <alpha-value>)',
+            content: 'rgba(var(--color-error-content, <alpha-value>)',
           },
         },
         spacing: {
-          // [`${prefix}header`]: '3rem',
-          // [`${prefix}main`]: `calc(100vh - theme(spacing.${prefix}header))`,
-          [`${prefix}cell-xs`]: '3rem',
-          [`${prefix}cell-sm`]: '4rem',
-          [`${prefix}cell-md`]: '5rem',
-          [`${prefix}cell-lg`]: '6rem',
-          [`${prefix}cell-xl`]: '7rem',
+          cell: 'var(--size-cell, 3rem)',
+          cellEtc: 'calc(100vh - theme(spacing.cell))',
+        },
+        borderRadius: {
+          box: 'var(--rounded-box, 0.75rem)',
+          btn: 'var(--rounded-btn, 0.5rem)',
+          badge: 'var(--rounded-badge, 0.25rem)',
         },
       },
     },
