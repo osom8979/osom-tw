305dc1283d994a6e1a4110c4e2c6ffd801e91f7b
zer0 <osom8979@gmail.com>
2024-03-17 20:44:02 +0900

    Update component: btn

--- src/tailwind/components/btn.ts
+++ src/tailwind/components/btn.ts
@@ -1,9 +1,33 @@
 export default () => {
   return {
+    '@keyframes btn-pop': {
+      '0%': {
+        transform: 'scale(var(--btn-focus-scale, 0.98));',
+      },
+      '40%': {
+        transform: 'scale(1.02);',
+      },
+      '100%': {
+        transform: 'scale(1);',
+      },
+    },
     '.btn': {
-      display: 'flex',
+      height: '3rem',
+      minHeight: '3rem',
+      paddingLeft: '1rem',
+      paddingRight: '1rem',
+      fontSize: '0.875rem',
+      flexShrink: '0',
+      cursor: 'pointer',
+      userSelect: 'none',
+      flexWrap: 'wrap',
+      borderColor: 'transparent',
+      textAlign: 'center',
+
+      display: 'inline-flex',
       alignItems: 'center',
       justifyContent: 'center',
+      '@apply rounded-btn': '',
 
       '&-primary': {
         display: 'static',
@@ -13,6 +37,41 @@ export default () => {
       },
       '&-outline': {
         display: 'static',
+      },
+
+      '&-xs': {
+        height: '1.5rem',
+        minHeight: '1.5rem',
+        paddingLeft: '0.5rem',
+        paddingRight: '0.5rem',
+        fontSize: '0.75rem',
+      },
+      '&-sm': {
+        height: '2rem',
+        minHeight: '2rem',
+        paddingLeft: '0.75rem',
+        paddingRight: '0.75rem',
+        fontSize: '0.875rem',
+      },
+      '&-md': {
+        height: '3rem',
+        minHeight: '3rem',
+        paddingLeft: '1rem',
+        paddingRight: '1rem',
+        fontSize: '0.875rem',
+      },
+      '&-lg': {
+        height: '4rem',
+        minHeight: '4rem',
+        paddingLeft: '1.5rem',
+        paddingRight: '1.5rem',
+        fontSize: '1.125rem',
+      },
+      '&-wide': {
+        width: '16rem',
+      },
+      '&-block': {
+        width: '100%',
       },
     },
   };
