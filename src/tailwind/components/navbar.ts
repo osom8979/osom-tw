4115e4594b4751dc2400864cb6431c2c65a36a9e
zer0 <osom8979@gmail.com>
2024-03-18 17:04:59 +0900

    Add icons: osom.logo.svg, github.svg

--- src/tailwind/components/navbar.ts
+++ src/tailwind/components/navbar.ts
@@ -3,7 +3,7 @@ export default () => {
     '.navbar': {
       display: 'flex',
       alignItems: 'center',
-      justifyContent: 'between',
+      justifyContent: 'space-between',
     },
   };
 };
